179f2a95a34bea54f5276d6c4458d645c9dde8dd
MERIDJI Sofiane <meridji.sofiane@outlook.com>
2022-01-26 12:00:18 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">MERIDJI Sofiane Portfolio!</a>
+          Welcome to <a href="https://nextjs.org">MERIDJI Sofiane Portfolio !</a>
         </h1>
       </main>
 
