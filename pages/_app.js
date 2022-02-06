cb914c4e9ac26ac14bd2b752ccc5c074f480fd3e
SOFIANE MERIDJI <meridji.sofiane@outlook.com>
2022-02-06 19:01:42 +0100

    add loder

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import Router from 'next/router'
+import {useState} from 'react'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import { BallTriangle } from  'react-loader-spinner'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading]= useState(false)
+  Router.events.on('routeChangeStart', ()=>setLoading(true))
+
+  Router.events.on('routeChangeComplete', ()=>setLoading(false))
+
+  return (
+  <>
+    {loading && <div style={{width: '100vw', height: '100vh',
+      justifyContent: 'center', display: 'flex',
+      alignItems: 'center', backgroundColor: '#222831', position: 'absolute', zIndex: 2 }}>
+      <BallTriangle color="#ffd269" height={80} width={80} />
+      </div>}
+    <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
